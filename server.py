ff677b4857b253f991c7303cd46377434f3398a0
Jonathan Young <jonyoungg@gmail.com>
2019-06-12 14:26:10 -0500

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import controller
 import socket
 
 # create a server listening on port 8888
-HOST, PORT = 'localhost', 8887
+HOST, PORT = 'localhost', 8888
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.bind((HOST, PORT))
